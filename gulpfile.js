cf536a3a7ab1b9b77dabde8830973603a059f15e
manu <manu9576@hotmail.fr>
2019-10-26 19:22:01 +0200

    gulp task completed

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,10 @@ const gulp = require('gulp');
 const plugins = require('gulp-load-plugins')();
 
 var source = './src';
-var desitnation = './dist';
-
-
+var destination = './dist';
 
 gulp.task('css', function () {
     return gulp.src(source + '/assets/css/styles.less')
       .pipe(plugins.less())
-      .pipe(gulp.dest(destiantion + '/assets/css'));
+      .pipe(gulp.dest(destination + '/assets/css/'));
   });
